bef6d17ff01cb041b86c3fca660bc9392323a552
Beau <beausouparphone@gmail.com>
2025-04-03 17:25:39 -0400

    Mount Navbar to Home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Navbar from '../../navbar/Navbar'
 
 export const Home = () => {
   return (
     <div className='home'>
-        
+        <Navbar />
     </div>
   )
 }
