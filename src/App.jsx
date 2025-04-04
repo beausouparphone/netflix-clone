0e45ac3b1a6fe514ccc3cce6365aa6956b6f505b
Beau <beausouparphone@gmail.com>
2025-04-04 01:53:15 -0400

    Add player to Player and mount to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Home } from './components/pages/home/Home'
 import { Routes, Route } from 'react-router-dom'
 import { Login } from './components/pages/login/Login'
+import { Player } from './components/pages/player/Player'
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/player/:id' element={<Player />} />
       </Routes>
     </div>
   )
--- src/components/pages/player/Player.jsx
+++ src/components/pages/player/Player.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Player.css'
+import back_arrow from '../../../assets/back.png'
 
 export const Player = () => {
   return (
     <div className='player'>
-
+        <img src={back_arrow} alt="" />
+        <iframe 
+            width='90%' 
+            height='90%'
+            src='https://www.youtube.com/embed/oJUz-s5PhsU'
+            title='trailer'
+            allowFullScreen
+        ></iframe>
+        <div className="player info">
+            <p>Published Date</p>
+            <p>Name</p>
+            <p>Type</p>
+        </div>
     </div>
   )
 }
