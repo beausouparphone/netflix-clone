f40a100688f60000bcd7b048ffc12813096f2ab7
Beau <beausouparphone@gmail.com>
2025-04-04 00:11:36 -0400

    Add Routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Home } from './components/pages/home/Home'
+import { Routes, Route } from 'react-router-dom'
+import { Login } from './components/pages/login/Login'
 
 export const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   )
 }
