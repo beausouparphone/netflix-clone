a0ccf4e725ed9801333bffa6a7043c323e752d87
Beau <beausouparphone@gmail.com>
2025-04-03 21:53:50 -0400

    Add banner to Home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Home.css'
 import { Navbar } from '../../navbar/Navbar'
+import banner_image from '../../../assets/banner-1.jpg'
+import banner_title from '../../../assets/arcane-logo.png'
 
 export const Home = () => {
   return (
     <div className='home'>
         <Navbar />
+        <div className="banner">
+            <img src={banner_image} alt="" className='banner-img'/>
+            <div className="banner-caption">
+                <img src={banner_title} alt="" className='caption-img'/>
+                <p>The origins of two iconic League champions, set in the utopian Piltover and the oppressed underground of Zaun.</p>
+            </div>
+        </div>
     </div>
   )
 }
