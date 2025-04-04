5be8e19806d54568782c2dc474dbfc45d4b9afc7
Beau <beausouparphone@gmail.com>
2025-04-04 13:52:50 -0400

    Add effect for login to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Home } from './components/pages/home/Home'
 import { Routes, Route } from 'react-router-dom'
 import { Login } from './components/pages/login/Login'
 import { Player } from './components/pages/player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 
 export const App = () => {
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log('Logged In');
+      } else {
+        console.log('Logged Out');
+      }
+    })
+  }, [])
+
   return (
     <div>
       <Routes>
