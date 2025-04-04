f3d562acbeb2138ce704490abef90285bc87080d
Beau <beausouparphone@gmail.com>
2025-04-04 00:59:57 -0400

    Add sign form-switch to Login

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../../assets/logo.png'
 
 export const Login = () => {
+
+  const [signState, setSignState] = useState('Sign In');
+
   return (
     <div className='login'>
         <img src={logo} alt=""className='login-logo' />
         <div className="login-form">
-          <h1>Sign Up</h1>
+          <h1>{signState}</h1>
           <form>
-            <input type="text" placeholder='Name'/>
+            {signState === 'Sign Up' ? 
+              <input type="text" placeholder='Name'/> : <></>}
             <input type="email" placeholder='Email'/>
             <input type="password" placeholder='Password'/>
-            <button>Sign In</button>
+            <button>{signState}</button>
             <div className="form-help">
               <div className="remember">
                 <input type="checkbox" />
@@ -21,6 +25,11 @@ export const Login = () => {
               <p>Need Help?</p>
             </div>
           </form>
+          <div className="form-switch">
+            {signState === 'Sign In' ? 
+              <p>New to Beauflix? <span onClick={() => {setSignState('Sign Up')}}>Sign Up Now</span></p> 
+              : <p>Already have an account? <span onClick={() => {setSignState('Sign In')}}>Sign In</span></p>}
+          </div>
         </div>
     </div>
   )
