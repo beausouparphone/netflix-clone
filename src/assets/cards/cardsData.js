6b83bdaa22216e4ca9eea1bba0c00470f37050b2
Beau <beausouparphone@gmail.com>
2025-04-03 22:58:35 -0400

    Add imgs and cardsData placeholder for TitleCards

---
+++ src/assets/cards/cardsData.js
@@ -0,0 +1,74 @@
+import card_1 from './Attack-on-Titan.png';
+import card_2 from './Chainsaw-Man.png';
+import card_3 from './Demon-Slayer.png';
+import card_4 from './Frieren.png';
+import card_5 from './Fullmetal-Alchemist.png';
+import card_6 from './HunterHunter.png';
+import card_7 from './Jujutsu-Kaisen.png';
+import card_8 from './My-Hero.png';
+import card_9 from './Naruto-Shippuden.png';
+import card_10 from './One-Piece.png';
+import card_11 from './Solo-Leveling.png';
+import card_12 from './Sword-Art-Online.png';
+import card_13 from './Tokyo-Ghoul.png';
+import card_14 from './Vinland-Saga.png';
+
+export const cards_data = [
+    {
+     image: card_1,
+     name: 'Attack on Titan'   
+    },
+    {
+     image: card_2,
+     name: 'Chainsaw Man'   
+    },
+    {
+     image: card_3,
+     name: 'Demon Slayer'   
+    },
+    {
+     image: card_4,
+     name: 'Frienren'   
+    },
+    {
+     image: card_5,
+     name: 'Fullmetal Alchemist'   
+    },
+    {
+     image: card_6,
+     name: 'Hunter x Hunter'   
+    },
+    {
+     image: card_7,
+     name: 'Jujutsu Kaisen'   
+    },
+    {
+     image: card_8,
+     name: 'My Hero Academia'   
+    },
+    {
+     image: card_9,
+     name: 'Naruto Shippuden'   
+    },
+    {
+     image: card_10,
+     name: 'One Piece'   
+    },
+    {
+     image: card_11,
+     name: 'Solo Leveling'   
+    },
+    {
+     image: card_12,
+     name: 'Sword Art Online'   
+    },
+    {
+     image: card_13,
+     name: 'Tokyo Ghoul'   
+    },
+    {
+     image: card_14,
+     name: 'Vinland Saga'   
+    },
+
+]
