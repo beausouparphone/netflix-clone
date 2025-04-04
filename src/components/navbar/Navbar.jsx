9567f6bbd259add8f3e5056c67be23878953598e
Beau <beausouparphone@gmail.com>
2025-04-03 21:24:51 -0400

    Add navbar-left and navbar-right to Narbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search.png'
+import bell_icon from '../../assets/bell.png'
+import login_img from '../../assets/login.png'
+import dropdown from '../../assets/dropdown.png'
+
+const categories = [
+    'Home',
+    'TV Shows',
+    'Movies',
+    'New & Popular',
+    'My List',
+    'Browse by Languages'
+]
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
-
+        <div className="navbar-left">
+            <img src={logo} alt="" />
+            <ul>
+                {categories.map((categoryName) => (
+                    <li>
+                        {categoryName}
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <div className="navbar-right">
+            <img src={search_icon} alt="" className='icons'/>
+            <p>family</p>
+            <img src={bell_icon} alt="" className='icons'/>
+            <div className="navbar-profile">
+                <img src={login_img} alt="" className='profile'/>
+                <img src={dropdown} alt="" />
+            </div>
+        </div>         
     </div>
   )
 }
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Home.css'
-import Navbar from '../../navbar/Navbar'
+import { Navbar } from '../../navbar/Navbar'
 
 export const Home = () => {
   return (
